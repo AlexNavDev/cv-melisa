840a8f6c2dae604e4b998c70812ce64a2ae77699
Alejandro <navarro.alejandro@outlook.es>
2026-04-21 16:05:30 -0600

    feat: change responsive Layout component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -4,8 +4,8 @@ import Footer from "@/components/footer/Footer";
 
 const Layout = () => {
   return (
-    <main>
-      <h1 className="h-16 pr-5 text-4xl flex items-center justify-end font-brassiere text-text-pink text-shadow-title-pink md:text-5xl lg:h-29.5 lg:text-[80px]">
+    <main className="w-82.5 mx-auto md:w-196.5 lg:w-310">
+      <h1 className="h-16 text-4xl flex items-center justify-end font-brassiere text-text-pink text-shadow-title-pink md:text-5xl lg:h-29.5 lg:text-[80px]">
         Melisa Oro
       </h1>
       <NavBar />
